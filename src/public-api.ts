131b0a8aa3f817c008148f68d70ac9152d5c6791
Arturo Alcalá <alcala28043@gmail.com>
2022-10-28 21:15:13 +0200

    remove initial component

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-mat-easy
  */
 
-export * from './lib/ngx-mat-easy.service';
-export * from './lib/ngx-mat-easy.component';
-export * from './lib/ngx-mat-easy.module';
+export * from "./lib/ngx-mat-easy.service";
+export * from "./lib/ngx-mat-easy.module";
